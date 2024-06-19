93e0ddd83abc675c27a37a4936aae123e9ae6bf7
Eren Yalçın <yalcineren97@gmail.com>
2024-06-20 02:24:56 +0300

    feat(home): use prompt endpoint to create simple portfolio html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-export default function Home() {
+async function getRandom() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/prompt`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
+
+export default async function Home() {
+  const { data } = await getRandom();
+
   return (
-    <main
-      className={"flex min-h-screen flex-col items-center justify-between p-24"}
-    >
-      {"welcome to chatchippidy"}
-    </main>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: data.choices[0].message.content as string,
+      }}
+    />
   );
 }
